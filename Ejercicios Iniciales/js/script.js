a2b015512cd0eb7ab759c9099b77fff9ef595fb2
CarlosEncisoUdeCaldas <carlosenciso.tic@ucaldas.edu.co>
2022-10-12 08:33:15 -0500

    script terminado ok

---
+++ Ejercicios Iniciales/js/script.js
@@ -0,0 +1,43 @@
+document.getElementById("insertar").innerHTML =
+  "Texto insertado desde un Script";
+
+// funcion para ocultar elementos
+// function ocultarElemento(){
+//   document.getElementById('hidden').style.display = 'none';
+// }
+
+//funcion para mostrar los elementos
+function mostrarElemento(valor) {
+  document.getElementById("hidden").style.display = valor;
+}
+
+//funcion para cambiar el tamaño
+function cambiarFont(valor) {
+  document.getElementById("estilo").style.fontSize = valor;
+}
+
+//funcion para cambiar el contenido
+function cambiarContenido(valor) {
+  document.getElementById("demo").innerHTML = valor;
+}
+
+//Encender y apagar un bombillo
+// function encendido() {
+//   document.getElementById("miImagen").src = "img/on.gif";
+// }
+
+// function apagado() {
+//     document.getElementById("miImagen").src = "img/off.gif";
+// }
+
+//la funcion encendido y apagado la puedo cambiar por unsa sola funcion con parametros
+function OnOff( ruta ){
+    document.getElementById("miImagen").src = ruta;
+}
+//sintaxis tipo flecha de la funcion apagado
+// const apagado = () => document.getElementById('miImagen').src = 'img/off.gif';
+
+// funcion unica del bombillo
+// function OnOff( valor ) {
+
+// }
